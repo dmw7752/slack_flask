16f5906947af0b4c41c8c0cd388f0bb97199e7a5
Dan White <dmw7752@gmail.com>
2015-02-06 14:49:01 -0800

    Fixing IP and port for heroku

--- app.py
+++ app.py
@@ -55,5 +55,5 @@ def stock():
   return response
 
 if __name__ == '__main__':
-  #port = int(os.environ.get("PORT", 5000))
-  app.run(host='127.0.0.1', port=5000)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
