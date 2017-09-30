f9e2848d94447dc611bb41ad23bd33cdc7459d48
Dan White <dawhite@dawhite-mn2.linkedin.biz>
2017-09-29 17:02:27 -0700

    Checking to see if we are running in heroku. If not enable debug and local ports.

--- app.py
+++ app.py
@@ -42,7 +42,9 @@ def stock():
   return response
 
 if __name__ == '__main__':
-  port = int(os.environ.get("PORT", 5000))
-  app.run(host='0.0.0.0', port=port)
-  #app.debug = True
-  #app.run(host='127.0.0.1', port=5000)
+  if os.environ.get('RUNNING_IN_HEROKU'):
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+  else:
+    app.debug = True
+    app.run(host='127.0.0.1', port=5000)
